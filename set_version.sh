52df108b1440532e893227e78d258a22d68443b4
Sebastiaan Mannem <sebas@mannem.nl>
2022-03-11 11:29:07 +0100

    Small error in set_version

--- set_version.sh
+++ set_version.sh
@@ -4,4 +4,4 @@ if [ -z "${VERSION}" ]; then
   VERSION=$(git tag | sort -V | grep '^v' | tail -n1)-devel
 fi
 
-sed -i "s/__version__ *=.*/__version__ = '$VERSION'" chainsmith/__init__.py
+sed -i "s/__version__ *=.*/__version__ = '${VERSION}'/" chainsmith/__init__.py
